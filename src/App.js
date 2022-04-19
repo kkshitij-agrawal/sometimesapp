ee69e117144f040f44c79ffcb017d7d83746f2b4
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2022-04-19 17:56:56 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
   }
 
   const [waterMarkFlag, setWaterMarkFlag] = useState(false);
-  const [quoteIndex, setquoteIndex] = useState(0);
+  var rand = Math.floor(Math.random() * 20);
+  const [quoteIndex, setquoteIndex] = useState(rand);
 
   function removeWatermark() {
     var card = document.getElementById("quoteBody");
@@ -43,24 +44,77 @@ function App() {
     removeWatermark();
   }
 
+  // {quote, textcolor, bgcolor}
   const quoteArray = [
-    "a",
-    "b",
-    "c",
-    "one small decision feels like the biggest decision of your life.",
+    ["it's just better to let things be.", "#d13642", "#fdedb2"],
+    ["it's all a scam.", "#ffd15e", "#009bc6"],
+    ["wasting time is more productive.", "#1539CF", "#F1D3D3"],
+    [
+      "one small decision feels like the biggest decision of your life.",
+      "#28292B",
+      "#B1FDEA",
+    ],
+    [
+      "one decision feels too small to be the biggest decision of your life.",
+      "#FF0046",
+      "#28292B",
+    ],
+    ["no-music workout is what you need.", "red", "yellow"],
+    ["coffee is all you need.", "red", "yellow"],
+    ["caffeine hits the g-spot.", "red", "yellow"],
+    [
+      "what didn't work out for you really worked out for you.",
+      "red",
+      "yellow",
+    ],
+    ["it's all the time.", "red", "yellow"],
+    ["enough is not enough.", "red", "yellow"],
+    ["silence says a lot more than words.", "red", "yellow"],
+    ["listening to a song on loop makes everything go away.", "red", "yellow"],
+    ["taking a nap makes everything go away.", "red", "yellow"],
+    ["", "red", "yellow"],
   ];
 
+  // {
+  //   "red&black":["#eree", "#erg"]
+  // }
+
   function quoteChange() {
-    // console.log(quoteIndex, quoteArray.length);
+    // while (true) {
+    //   rand = Math.floor(Math.random() * 20);
+    //   if (rand < quoteArray.length) {
+    //     setquoteIndex(rand);
+    //     break;
+    //   }
+    // }
+    console.log(
+      quoteArray[quoteIndex][0],
+      quoteArray[quoteIndex][1],
+      quoteArray[quoteIndex][2]
+    );
     if (quoteIndex >= quoteArray.length - 1) {
       setquoteIndex(0);
     } else {
       setquoteIndex(quoteIndex + 1);
     }
+
+    var appColors = document.getElementById("App");
+    // var appColors = card.getElementsByClassName("watermark")[0];
+    // card.removeChild(waterMark);
+    var downloadBtn = document.getElementById("downloadBtn");
+    var tweetBtn = document.getElementById("tweetBtn");
+    appColors.style.color = `${quoteArray[quoteIndex][1]}`;
+    appColors.style.backgroundColor = `${quoteArray[quoteIndex][2]}`;
+    downloadBtn.style.color = `${quoteArray[quoteIndex][2]}`;
+    downloadBtn.style.backgroundColor = `${quoteArray[quoteIndex][1]}`;
+    downloadBtn.style.border = `solid ${quoteArray[quoteIndex][1]}`;
+    tweetBtn.style.border = `solid ${quoteArray[quoteIndex][1]}`;
+    tweetBtn.style.color = `${quoteArray[quoteIndex][1]}`;
+    tweetBtn.style.opacity = ".7";
   }
 
   return (
-    <div className="App">
+    <div className="App" id="App">
       {/* {quoteArray.map((quote) => ( */}
       <div>
         <header className="App-header">
@@ -71,9 +125,10 @@ function App() {
             <p></p>
             <a
               className="App-link"
-              href={`https://twitter.com/intent/tweet?url=sometimesin.life&text=sometimes, ${quoteArray[quoteIndex]}%0A- `}
+              href={`https://twitter.com/intent/tweet?url=sometimesin.life&text=sometimes, ${quoteArray[quoteIndex][0]}%0A- `}
               target="_blank"
               rel="noopener noreferrer"
+              id="tweetBtn"
             >
               Tweet
             </a>
@@ -84,6 +139,7 @@ function App() {
               }}
               target="_blank"
               rel="noopener noreferrer"
+              id="downloadBtn"
             >
               Download as PNG
             </a>
@@ -93,7 +149,7 @@ function App() {
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p className="mainText">
             <div className="smallText">sometimes,</div>
-            {quoteArray[quoteIndex]}
+            {quoteArray[quoteIndex][0]}
           </p>
           {/* <a
           className="App-link"
