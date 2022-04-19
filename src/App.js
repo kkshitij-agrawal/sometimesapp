c4c40cc069f37d5edc3f048fd599056a7a0abc98
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2022-04-19 11:04:57 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { React, useState } from "react";
+
+import "./App.css";
+import "font-awesome/css/font-awesome.min.css";
+import html2canvas from "html2canvas";
 
 function App() {
+  function download(dataurl, filename) {
+    const link = document.createElement("a");
+    link.href = dataurl;
+    link.download = filename;
+    link.click();
+  }
+
+  const [waterMarkFlag, setWaterMarkFlag] = useState(false);
+  const [quoteIndex, setquoteIndex] = useState(0);
+
+  function removeWatermark() {
+    var card = document.getElementById("quoteBody");
+    if (waterMarkFlag) {
+      var waterMark = card.getElementsByClassName("watermark")[0];
+      card.removeChild(waterMark);
+      setWaterMarkFlag(!waterMarkFlag);
+      waterMark.innerHTML = "";
+    }
+  }
+
+  function downloadPNG() {
+    var card = document.getElementById("quoteBody");
+    if (!waterMarkFlag) {
+      var waterMark = document.createElement("div");
+      waterMark.className = "watermark";
+      waterMark.innerHTML = "sometimesin.life";
+      card.appendChild(waterMark);
+      setWaterMarkFlag(!waterMarkFlag);
+    }
+    html2canvas(card, { useCORS: true }).then(function (canvas) {
+      var img = canvas.toDataURL("image/png");
+      download(img, "sometimes.png");
+    });
+
+    removeWatermark();
+  }
+
+  const quoteArray = [
+    "a",
+    "b",
+    "c",
+    "one small decision feels like the biggest decision of your life.",
+  ];
+
+  function quoteChange() {
+    // console.log(quoteIndex, quoteArray.length);
+    if (quoteIndex >= quoteArray.length - 1) {
+      setquoteIndex(0);
+    } else {
+      setquoteIndex(quoteIndex + 1);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+      {/* {quoteArray.map((quote) => ( */}
+      <div>
+        <header className="App-header">
+          <div className="leftHeader">
+            <p>sometimesin.life</p>
+          </div>
+          <div className="rightHeader">
+            <p></p>
+            <a
+              className="App-link"
+              href={`https://twitter.com/intent/tweet?url=sometimesin.life&text=sometimes, ${quoteArray[quoteIndex]}%0A- `}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Tweet
+            </a>
+            <a
+              className="App-link"
+              onClick={() => {
+                downloadPNG();
+              }}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Download as PNG
+            </a>
+          </div>
+        </header>
+        <div className="body" id="quoteBody">
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <p className="mainText">
+            <div className="smallText">sometimes,</div>
+            {quoteArray[quoteIndex]}
+          </p>
+          {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+
+          {/* <div className="watermark">
+          Made on
+          <a href="https://www.polycard.me"> Polycard.me</a>
+        </div> */}
+        </div>
+      </div>
+      {/* ))} */}
+      <i
+        className="fa fa-refresh fa-2xl"
+        id="refreshIcon"
+        aria-hidden="true"
+        onClick={() => {
+          quoteChange();
+        }}
+      ></i>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDvxAd0VNxktJqCv4BFuj3R3JSfS7DaAeM",
+//   authDomain: "sometimes-1e293.firebaseapp.com",
+//   projectId: "sometimes-1e293",
+//   storageBucket: "sometimes-1e293.appspot.com",
+//   messagingSenderId: "122508091018",
+//   appId: "1:122508091018:web:1b3d541e361df3d7899773",
+//   measurementId: "G-K04DGHPZ0Y",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
